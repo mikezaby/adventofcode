be670f8f73f54ae08265323986a3172396ce3b40
mikezaby <mikezaby@gmail.com>
2015-12-15 23:43:39 +0200

    [Day6] First part

---
+++ 06/6.rb
@@ -0,0 +1,44 @@
+class Lcd
+  attr_reader :file, :monitor
+
+  def initialize(file_path)
+    @file = File.new(file_path, "r")
+    @monitor = Array.new(1000) { Array.new(1000, false) }
+  end
+
+  def load_instractions
+    file.each_line do |line|
+      cmd, from, to = parse(line.chomp.split)
+      (from[1]..to[1]).each do |y|
+        (from[0]..to[0]).each do |x|
+          send(cmd, x, y)
+        end
+      end
+    end
+  end
+
+  def parse(tokens)
+    [command(tokens), tokens.first.split(',').map(&:to_i), tokens.last.split(',').map(&:to_i)]
+  end
+
+  def command(tokens)
+    cmd = tokens.shift
+    cmd == 'toggle' ? cmd : tokens.shift
+  end
+
+  def on(x, y)
+    monitor[y][x] = true
+  end
+
+  def off(x, y)
+    monitor[y][x] = false
+  end
+
+  def toggle(x, y)
+    monitor[y][x] = !monitor[y][x]
+  end
+end
+
+lcd = Lcd.new('6.data')
+lcd.load_instractions
+puts lcd.monitor.flatten.count(&:itself)
