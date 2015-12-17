80d1d5a554e3e94d5a82409fad0962153ffd0606
mikezaby <mikezaby@gmail.com>
2015-12-17 21:19:18 +0200

    [Day8] Part 2

--- 08/8.rb
+++ 08/8.rb
@@ -1,7 +1,8 @@
 require '../file_helper.rb'
 
 diff = FileHelper.new('8.data').reduce(0) do |diff, line|
-  diff += line.chomp.length - eval(line).length
+  line.chomp!
+  diff += line.gsub("\\", "\\\\\\").gsub('"', '\"').length - line.chomp.length + 2
 end
 
 puts diff
