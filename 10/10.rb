26e2acdf9583758362b7638f39be8e916963a960
mikezaby <mikezaby@gmail.com>
2015-12-19 00:10:51 +0200

    [Day10] First part

---
+++ 10/10.rb
@@ -0,0 +1,21 @@
+def look_and_say(string)
+  char_counts = []
+
+  string.chars.each do |new_char|
+    char_count = char_counts.last
+
+    if char_count && char_count.last == new_char
+      char_count[0] += 1
+    else
+      char_counts.push [1, new_char]
+    end
+  end
+
+  char_counts.join
+end
+
+final = 40.times.reduce('1113222113') do |string|
+  look_and_say(string)
+end
+
+puts final.length
