b84501e7df996311776d3d0b09f5d81402352506
mikezaby <mikezaby@gmail.com>
2015-12-19 20:12:19 +0200

    [Day14] Part 2

--- 14/14.rb
+++ 14/14.rb
@@ -2,24 +2,35 @@ require '../file_helper.rb'
 
 class Reindeer
   attr_reader :name, :kms, :duration, :rest
+  attr_accessor :points, :last_distance
 
   def initialize(name, kms, duration, rest)
     @name = name
     @kms = kms.to_i
     @duration = duration.to_i
     @rest = rest.to_i
+    @points = 0
+    @last_distance
   end
 
   def distance(seconds)
     rounds, remaining = seconds.divmod(duration+rest)
     extra_km = [remaining, duration].min * kms
-    rounds * duration * kms + extra_km
+    self.last_distance = rounds * duration * kms + extra_km
+  end
+
+  def get_point
+    self.points += 1
   end
 end
 
-distances = FileHelper.new('14.data').map do |line|
+reindeers = FileHelper.new('14.data').map do |line|
   tokens = line.split
-  Reindeer.new(tokens[0], tokens[3], tokens[6], tokens[13]).distance(2503)
+  Reindeer.new(tokens[0], tokens[3], tokens[6], tokens[13])
 end
 
-puts distances.max
+(1..2503).each do |seconds|
+  grouped = reindeers.group_by { |r| r.distance(seconds) }.sort.last.last.each(&:get_point)
+end
+
+puts reindeers.group_by(&:points).sort.last.last.max_by(&:last_distance).points
