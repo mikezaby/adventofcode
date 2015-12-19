e772d6593504da3dca39348b6b2ef0c4a1499f00
mikezaby <mikezaby@gmail.com>
2015-12-19 16:17:33 +0200

    [Day12] Part 2

--- 12/12.rb
+++ 12/12.rb
@@ -3,7 +3,9 @@ require 'json'
 
 def extract_nums(input)
   case input
-  when Hash then extract_nums(input.values)
+  when Hash then
+    values = input.values
+    values.include?("red") ? 0 : extract_nums(values)
   when Array then input.reduce(0) { |sum, v| sum + extract_nums(v) }
   when Integer then input
   else 0
