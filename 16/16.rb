497b1ad3a5637a433b119557f9473e5ef36b766f
mikezaby <mikezaby@gmail.com>
2015-12-20 17:07:17 +0200

    [Day16] First part

---
+++ 16/16.rb
@@ -0,0 +1,6 @@
+require '../file_helper'
+
+requirements = eval(FileHelper.new('16.gift.data').to_a.join)
+aunts = eval(FileHelper.new('16.data').to_a.join)
+
+puts aunts.find { |_, value| value.all? { |k, v| requirements[k] == v } }.first
