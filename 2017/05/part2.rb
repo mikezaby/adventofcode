5357a17144f40cc085cccecfd73e8d71a96ee100
Michalis Zamparas <mikezaby@skroutz.gr>
2017-12-08 19:51:42 +0200

    Solution 05 - Part 2 (ruby)

---
+++ 2017/05/part2.rb
@@ -0,0 +1,17 @@
+instructions = File.read('2017/05/input.txt').each_line.map(&:to_i)
+
+instructions_count = instructions.count
+
+offset = 0
+step = 0
+
+loop do
+  new_offset = offset + instructions[offset]
+  step += 1
+
+  break puts(step) if new_offset >= instructions_count || new_offset < 0
+
+  instructions[offset] += instructions[offset] >= 3 ? -1 : 1
+
+  offset = new_offset
+end
