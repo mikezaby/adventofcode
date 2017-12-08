bcf4279c1cd2b40c576e2bd71a1d00dbd6c43a38
Michalis Zamparas <mikezaby@skroutz.gr>
2017-12-08 13:24:46 +0200

    Fix issue with even perfect squares

--- 2017/03/part1.rb
+++ 2017/03/part1.rb
@@ -3,8 +3,11 @@ my_square = 312051
 width = Math.sqrt(my_square).ceil
 width += 1 if width.even?
 
-radius = (width - 1) / 2
+radius = width / 2
 
 last_square = width ** 2
 
-(((last_square - my_square) % width) - radius).abs + radius
+sides_behind = (last_square - my_square) / (width - 1)
+last_side_square = last_square - sides_behind * (width - 1)
+
+((last_side_square - my_square) - radius).abs + radius
