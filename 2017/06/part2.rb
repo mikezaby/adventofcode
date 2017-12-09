82c472d6b8af08d97e6f241574eaf77355cf409f
Michalis Zamparas <mikezaby@gmail.com>
2017-12-09 16:35:08 +0200

    Solution 06 - Part 2 (ruby)

---
+++ 2017/06/part2.rb
@@ -0,0 +1,21 @@
+require 'set'
+
+memory_banks = File.read('2017/06/input.txt').split("\t").map(&:to_i)
+banks_count = memory_banks.count
+state = Set.new([memory_banks])
+
+loop do
+  memory_banks = memory_banks.dup
+
+  max_blocks = memory_banks.max
+  bank_index = memory_banks.index(max_blocks)
+  memory_banks[bank_index] = 0
+
+  ((bank_index + 1)..(bank_index + max_blocks)).each { |i| memory_banks[i % banks_count] += 1 }
+
+  if !state.add?(memory_banks)
+    p memory_banks
+    p state
+    break puts(state.count - state.find_index(memory_banks))
+  end
+end
