5ed2cca951cf517a3cdb8348ed1b03427020357f
Michalis Zamparas <mikezaby@gmail.com>
2020-12-04 15:18:07 +0200

    Update runner

--- runner.rb
+++ runner.rb
@@ -5,28 +5,20 @@ require './file_helper'
 
 @_init_constants = Object.constants.dup
 
-def advent(*current_advent)
+def run(part = 1, date = Date.today)
   @output = nil
 
   (Object.constants - @_init_constants).each do |const|
     Object.send(:remove_const, const)
   end
 
-  if current_advent.any?
-    @year, @day, @part = current_advent
-  end
-
-  base_path = "20#{@year}/#{@day}"
+  base_path = [date.year, date.day.to_s.rjust(2, "0")].join("/")
   data_path = [base_path, "data.txt"].join("/")
-  code_path = [base_path, "part#{@part}.rb"].join("/")
+  code_path = [base_path, "part#{part}.rb"].join("/")
 
   @data = File.exist?(data_path) ? FileHelper.new(data_path) : nil
 
   load code_path
 
   @output
-end
-
-def reload!
-  advent
 end
