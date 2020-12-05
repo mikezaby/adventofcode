2a9422e0002a023fe17979697654d8e89371abbe
Michalis Zamparas <mikezaby@gmail.com>
2020-12-05 16:30:26 +0200

    Add support for demo file

--- runner.rb
+++ runner.rb
@@ -15,9 +15,11 @@ def run(part = 1, date = Date.today)
 
   base_path = [date.year, date.day.to_s.rjust(2, "0")].join("/")
   data_path = [base_path, "data.txt"].join("/")
+  demo_path = [base_path, "demo.txt"].join("/")
   code_path = [base_path, "part#{part}.rb"].join("/")
 
   @data = File.exist?(data_path) ? FileHelper.new(data_path) : nil
+  @demo = File.exist?(demo_path) ? FileHelper.new(demo_path) : nil
 
   load code_path
 
